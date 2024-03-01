1bc42ffe1470e084ddbfb32643c8c666d47927e4
Zue <73200512+vizarodi@users.noreply.github.com>
2024-03-01 12:12:26 -0300

    clock ready (need more style)

---
+++ Assets/Scripts/GameManager/Timemanager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timemanager : MonoBehaviour
+{
+    public static Timemanager Instance { get; private set; }
+
+
+    [SerializeField] TextMeshProUGUI timerText;
+    float tiempoTranscurrido;
+    void Update()
+    {
+        if(timerText.text != "00:03")
+        { 
+            tiempoTranscurrido += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(tiempoTranscurrido / 60);
+            int seconds = Mathf.FloorToInt(tiempoTranscurrido % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        }
+       
+    }
+}
